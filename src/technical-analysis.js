5ab1ef72a5cf28eff653d1f1cc152234bc1e14ba
Nera Liu <neraliu@gmail.com>
2015-02-28 22:01:54 +0800

    add the bollinger bands method.

--- src/technical-analysis.js
+++ src/technical-analysis.js
@@ -14,6 +14,65 @@ function TechnicalAnalysis() {
 }
 
 /**
+* @function module:technical-analysis#_standardDeviation
+*/
+TechnicalAnalysis.prototype._standardDeviation = function(d, m, t) {
+    var sum = 0;
+    var sqrDiffs = d.map(function(v) {
+        var diff = v - m;
+        return diff*diff;
+    });
+    sum = sqrDiffs.reduce(function(p, c) {
+        return p + c;
+    });
+    return Math.sqrt(sum/t);
+};
+
+/**
+* @function module:technical-analysis#bollingerBands
+*
+* @param {Array} d - the array of data.
+* @param {int} t - the moving window of moving average.
+* @param {int} std - the standard deviation.
+* @returns {Array} - the result.
+*
+*/
+TechnicalAnalysis.prototype.bollingerBands = function(d, t, std) {
+    if (d.length >= t && d.constructor === Array) {
+        var r = {},
+            stds = [], f = this.decimalRoundingFactor;
+
+        r.r1 = [];
+        r.r2 = this.movingAverage(d, t, false);
+        r.r3 = [];
+
+        var ta = this;
+        r.r2.map(function(v, i) {
+            if (!isNaN(v)) {
+                var data = d.slice(i+1-t, i+1);
+                var std = ta._standardDeviation(data, v, t);
+                stds.push(std);
+            } else {
+                stds.push(NaN);
+            }
+        });
+
+        r.r2.map(function(v, i) {
+            // upper band
+            r.r1.push(Math.round((v+2*stds[i])*f)/f);
+            // middle band
+            r.r2[i] = Math.round(r.r2[i]*f)/f;
+            // lower band
+            r.r3.push(Math.round((v-2*stds[i])*f)/f);
+        });
+
+        return r;
+    } else {
+        throw "[ERROR] TechnicalAnalysis#bollingerBands: Not enought data! OR data is not Array!";
+    }
+};
+
+/**
 * @function module:technical-analysis#_expoentialMovingAverage
 */
 TechnicalAnalysis.prototype._exponentialMovingAverage = function(d, t, i, m, pema, r) {
@@ -65,24 +124,27 @@ TechnicalAnalysis.prototype.exponentialMovingAverage = function(d, t) {
 *
 * @param {Array} d - the array of data.
 * @param {int} t - the moving window of moving average.
+* @param {boolean} roundUp - round up the result?
 * @returns {Array} - the result.
 *
 */
-TechnicalAnalysis.prototype.movingAverage = function(d, t) {
+TechnicalAnalysis.prototype.movingAverage = function(d, t, roundUp) {
     if (d.length >= t && d.constructor === Array) {
         var r = [],
             s = 0, f = this.decimalRoundingFactor, ma;
 
+        roundUp = typeof roundUp === undefined? true : roundUp;
+
         for(var i=0;i<d.length;++i) {
-            s += d[i];
+            s += isNaN(d[i])? 0: d[i];
             if (i < t-1) {
                 r.push(NaN);
             } else if (i+1 === t) {
-                ma = Math.round((s/t)*f)/f;
+                ma = roundUp? Math.round((s/t)*f)/f: s/t;
                 r.push(ma);
             } else {
-                s -= d[i-t];
-                ma = Math.round((s/t)*f)/f;
+                s -= isNaN(d[i-t])? 0: d[i-t];
+                ma = roundUp? Math.round((s/t)*f)/f: s/t;
                 r.push(ma);
             }
         }
