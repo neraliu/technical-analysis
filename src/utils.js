b56f902478a7f269fb63189cb93d6e8fbf21f836
Nera Liu <neraliu@gmail.com>
2015-04-15 14:01:54 +0800

    code clean up

--- src/utils.js
+++ src/utils.js
@@ -21,6 +21,39 @@ utils.writeFile = function(file, data) {
     fs.closeSync(f);
 };
 
+utils.saveBuyTransaction = function(stockid, price, volume, extra) {
+    var tran = {};
+    tran.stockid = stockid;
+    tran.buyPrice = price;
+    tran.status = 'open';
+    tran.cost = 9.99;
+    tran.volume = volume;
+    tran.marketValueBuyPrice = tran.buyPrice * tran.volume;
+    tran.extra = extra;
+    return tran;
+};
+
+/* we assume sell the transaction atomically */
+utils.saveSellTransaction = function(tran, price, volume, extra) {
+    tran.sellPrice = price;
+    tran.status = 'closed';
+    tran.cost += 9.99;
+    tran.volume -= volume;
+    tran.marketValueSellPrice = tran.sellPrice * volume;
+    tran.profitOrLoss = (tran.sellPrice-tran.buyPrice)*volume - tran.cost;
+    tran.return = (tran.profitOrLoss/tran.marketValueBuyPrice);
+
+    var o = tran.extra;
+    tran.extra = extra;
+    for (var key in o) {
+        if (o.hasOwnProperty(key)) {
+            tran.extra[key] = o[key];
+        }
+    }
+
+    return tran;
+};
+
 module.exports = utils;
 
 })();
